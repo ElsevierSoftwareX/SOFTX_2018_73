6434de7ada943f68a1d5994667ae4833b4505221
Miguel A. Rodriguez <miguelr@berkeley.edu>
2017-01-25 16:16:12 -0800

    Changed directory in which unit domain mesh files are saved.

--- test/generate_unit_domain_mesh_files.py
+++ test/generate_unit_domain_mesh_files.py
@@ -65,8 +65,8 @@ dim_str   = 'x'.join(['%i' % i for i in mesh_dims])
 # Save files
 print('Writing files.....', end='')
 
-mesh_name = './meshfiles/unit_domain-mesh-%s' % dim_str
-mesh_func_name = './meshfiles/unit_domain-mesh_function-%s' % dim_str
+mesh_name = './meshfiles/unit_domain/unit_domain-mesh-%s' % dim_str
+mesh_func_name = './meshfiles/unit_domain/unit_domain-mesh_function-%s' % dim_str
 
 if args.hdf5:
     mesh_name += '.h5'
