6b0b73eb885a91c55c4970421fb8e438a1b6783e
Miguel Rodriguez <miguelr@berkeley.edu>
2017-04-04 17:46:25 -0700

    Fixed bug in package initialization.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Import the objects necessary to use package.
 from .mechanicsproblem import MechanicsProblem
+from .solidmechanicsproblem import SolidMechanicsProblem
 from .mechanicssolver import MechanicsSolver
-from .solidmechanicssolver import SolidMechanicsSolver
 from .materials.solid_materials import *
 from .utils import *
