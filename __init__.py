545d580f84b3c69a941047e175a12dc75b825546
Miguel A. Rodriguez <miguelr@berkeley.edu>
2017-09-28 16:07:59 -0700

    Added check to __init__.py see if python 3 is being used since CBC-Block is not compatible.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,19 @@
 # Import the objects necessary to use package.
-from .mechanicsproblem import MechanicsProblem
 from .solidmechanics import SolidMechanicsProblem, SolidMechanicsSolver
-from .mechanicssolver import MechanicsSolver
 from .materials.solid_materials import *
 from .materials.fluids import *
 from .utils import *
+
+import sys as _sys
+if _sys.version_info[0] < 3:
+    from .mechanicsproblem import MechanicsProblem
+    from .mechanicssolver import MechanicsSolver
+else:
+    s = """
+    ***********************************************************
+    *                        WARNING:                         *
+    * The CBC-Block FEniCS App, which MechanicsSolver depends *
+    *           on, is not compatible with python 3.          *
+    ***********************************************************
+    """
+    print(s)
