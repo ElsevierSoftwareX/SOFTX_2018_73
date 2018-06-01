f157d9a4c85307c6580e19c2b1171e8784ceeb32
Miguel A. Rodriguez <miguelr@berkeley.edu>
2018-05-31 17:36:50 -0700

    Added hack with dummy imports to fix the import issues when using sphinx.

---
+++ .dummy_imports/README.txt
@@ -0,0 +1,3 @@
+This directory is a workaround/hack to get sphinx to work with GitLab
+CI to generate a page. I couldn't get the conda or the fenics docker
+images to work.
---
+++ .dummy_imports/dolfin/__init__.py
@@ -0,0 +1,20 @@
+class Dummy_mpi:
+    def __init__(self):
+        pass
+    def rank(*args):
+        return 0
+
+MPI = Dummy_mpi()
+parameters = {
+    'form_compiler': {
+        'cpp_optimize': None,
+        'representation': None,
+        'quadrature_degree': None,
+        'optimize': None
+    }
+}
+
+NonlinearVariationalSolver = object
+
+def mpi_comm_world():
+    return None
