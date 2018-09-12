ff1dc80b5cec58fbbcfbae5645e716df49b0c789
Miguel A. Rodriguez <miguelr@berkeley.edu>
2018-09-12 14:49:13 -0700

    Added parameter to generate config for unsteady problem.

--- test.tmp/conftest.py
+++ test.tmp/conftest.py
@@ -1,9 +1,6 @@
 import pytest
 
-# Need to figure out how to generate a default config dictionary based
-# on the class that it will be used for.
-
-def _default_config(class_name):
+def _default_config(class_name, unsteady=False):
     import fenicsmechanics as fm
 
     mat_dict = {
@@ -20,9 +17,7 @@ def _default_config(class_name):
     }
 
     formulation_dict = {
-        'time': {'unsteady': False},
         'element': "p2-p0",
-        # 'domain': "lagrangian",
         'bcs': {
             'dirichlet': {
                 'displacement': [[0.0]*3],
@@ -51,58 +46,23 @@ def _default_config(class_name):
 
         formulation_dict['domain'] = "lagrangian"
 
+    if unsteady:
+        formulation_dict['time'] = {
+            'unsteady': True,
+            'interval': [0., 1.],
+            'dt': 0.1
+        }
+    else:
+        formulation_dict['time'] = {'unsteady': False}
+
     config = {'material': mat_dict, 'mesh': mesh_dict,
               'formulation': formulation_dict}
 
     return config
 
 
+# Returns the function above used to generate a config dictionary based
+# on the problem class that will be used.
 @pytest.fixture
 def default_config():
     return _default_config
-
-# @pytest.fixture
-# def default_config():
-#     import fenicsmechanics as fm
-
-#     mat_dict = {
-#         'const_eqn': "lin_elastic",
-#         'type': "elastic",
-#         'incompressible': True,
-#         'density': 10.0,
-#         'la': 15.0,
-#         'mu': 5.0
-#     }
-
-#     mesh_file, boundaries_file = fm._get_mesh_file_names("unit_domain",
-#                                                          ret_facets=True,
-#                                                          refinements=[12]*3)
-#     mesh_dict = {
-#         'mesh_file': mesh_file,
-#         'boundaries': boundaries_file
-#     }
-
-#     formulation_dict = {
-#         'time': {'unsteady': False},
-#         'element': "p2-p0",
-#         'domain': "lagrangian",
-#         'bcs': {
-#             'dirichlet': {
-#                 'displacement': [[0.0]*3],
-#                 'velocity': [[0.0]*3],
-#                 'regions': [1],
-#                 # 'pressure': [0.0],
-#                 # 'p_regions': [1]
-#             },
-#             'neumann': {
-#                 'values': ["30.0*t", [0.0, 1.0, 0.0]],
-#                 'regions': [2, 2],
-#                 'types': ["pressure", "piola"]
-#             }
-#         }
-#     }
-
-#     config = {'material': mat_dict, 'mesh': mesh_dict,
-#               'formulation': formulation_dict}
-
-#     return config
