3bbb15cc35b70b534eba583433f7f0cf7cdfd32f
Miguel A. Rodriguez <miguelr@berkeley.edu>
2018-10-01 11:13:05 -0700

    Updated problem 1 script with argument parser.

--- test/manual/land_benchmark/problem1.py
+++ test/manual/land_benchmark/problem1.py
@@ -1,47 +1,81 @@
+import os
+import sys
+import argparse
 import dolfin as dlf
 import fenicsmechanics as fm
+
+# For use with emacs python shell.
+try:
+    sys.argv.remove("--simple-prompt")
+except ValueError:
+    pass
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--pressure",
+                    default=0.004, type=float,
+                    help="Pressure to be applied at z = 0.")
+
+mesh_dir = fm._get_mesh_file_names("beam", ret_dir=True, ret_mesh=False)
+mesh_file = os.path.join(mesh_dir, "beam_1000.h5")
+parser.add_argument("--mesh-file",
+                    type=str, default=mesh_file,
+                    help="Name of mesh file to use for mesh and facet function.")
+parser.add_argument("--generate-mesh",
+                    action="store_true", help="Generates mesh using mshr.")
+parser.add_argument("--resolution",
+                    default=70, type=int,
+                    help="Resolution used to generate mesh with mshr.")
+parser.add_argument("--incompressible",
+                    action="store_true", help="Model as incompressible material.")
+parser.add_argument("--bulk-modulus",
+                    type=float, default=1e3, dest="kappa",
+                    help="Bulk modulus of the material.")
+parser.add_argument("--loading-steps", "-ls",
+                    type=int, default=10,
+                    help="Number of loading steps to use.")
+parser.add_argument("--polynomial-degree", "-pd",
+                    type=int, default=2, dest="pd", choices=[1, 2, 3],
+                    help="Polynomial degree to be used for displacement.")
+args = parser.parse_args()
 
 # Region IDs
 HNBC  = 0  # homogeneous Neumann BC
 HDBC  = 10  # homogeneous Dirichlet BC
 INBC  = 20  # inhomogeneous Neumann BC
 
-# fname = "../../../meshfiles/beam/beam_1000.h5"
-# mesh = fm.load_mesh(fname)
+if args.generate_mesh:
+    import mshr
+    domain = mshr.Box(dlf.Point(), dlf.Point(10, 1, 1))
+    mesh_file = mshr.generate_mesh(domain, args.resolution)
 
-import mshr
-domain = mshr.Box(dlf.Point(), dlf.Point(10, 1, 1))
-mesh = mshr.generate_mesh(domain, 80)
+    boundaries = dlf.MeshFunction("size_t", mesh_file, 2)
+    boundaries.set_all(HNBC)
 
-boundaries = dlf.MeshFunction("size_t", mesh, 2)
-boundaries.set_all(HNBC)
+    hdbc = dlf.CompiledSubDomain("near(x[0], 0.0) && on_boundary")
+    hdbc.mark(boundaries, HDBC)
 
-hdbc = dlf.CompiledSubDomain("near(x[0], 0.0) && on_boundary")
-hdbc.mark(boundaries, HDBC)
-
-inbc = dlf.CompiledSubDomain("near(x[2], 0.0) && on_boundary")
-inbc.mark(boundaries, INBC)
+    inbc = dlf.CompiledSubDomain("near(x[2], 0.0) && on_boundary")
+    inbc.mark(boundaries, INBC)
+else:
+    mesh_file = boundaries = args.mesh_file
 
 mesh = {
-    # 'mesh_file': fname,
-    # 'boundaries': fname
-    'mesh_file': mesh,
+    'mesh_file': mesh_file,
     'boundaries': boundaries
 }
 
 cf = dlf.Constant([1., 0., 0.])
 cs = dlf.Constant([0., 1., 0.])
-
 material = {
     'type': 'elastic',
     'const_eqn': 'guccione',
-    'incompressible': True,
+    'incompressible': args.incompressible,
     'density': 0.0,
     'C': 2.0,
     'bf': 8.0,
     'bt': 2.0,
     'bfs': 4.0,
-    'kappa': 1e30,
+    'kappa': args.kappa,
     'fibers': {
         'fiber_files': [cf, cs],
         'fiber_names': ['e1', 'e2'],
@@ -49,13 +83,13 @@ material = {
     }
 }
 
+interval = [0., 1.]
+dt = (interval[1] - interval[0])/args.loading_steps
 formulation = {
     'time': {
-        'dt': 0.1,
-        'interval': [0., 1.]
+        'dt': dt,
+        'interval': interval
     },
-    'element': 'p2-p1',
-    # 'element': 'p1',
     'domain': 'lagrangian',
     'bcs': {
         'dirichlet': {
@@ -64,25 +98,37 @@ formulation = {
         },
         'neumann': {
             'regions': [INBC],
-            'values': ["0.004*t"],
+            'values': ["%f*t" % args.pressure],
             'types': ['pressure']
         }
     }
 }
 
+if args.incompressible:
+    formulation['element'] = 'p%i-p%i' % (args.pd, args.pd - 1)
+else:
+    formulation['element'] = 'p%i' % args.pd
+
 config = {'mesh': mesh, 'material': material, 'formulation': formulation}
 
+if args.incompressible:
+    fname_disp = "results/displacement-incompressible.pvd"
+    fname_pressure = "results/pressure.pvd"
+else:
+    fname_disp = "results/displacement.pvd"
+    fname_pressure = None
 problem = fm.SolidMechanicsProblem(config)
-solver = fm.SolidMechanicsSolver(problem, fname_disp="results/displacement.pvd")
+solver = fm.SolidMechanicsSolver(problem, fname_disp=fname_disp,
+                                 fname_pressure=fname_pressure)
 solver.full_solve()
 
-print("DOF(u) = ", problem.displacement.function_space().dim())
+rank = dlf.MPI.rank(dlf.mpi_comm_world())
+if rank == 0:
+    print("DOF(u) = ", problem.displacement.function_space().dim())
 
 import numpy as np
 vals = np.zeros(3)
 x = np.array([10., 0.5, 1.])
-# problem.displacement.set_allow_extrapolation(True)
-rank = dlf.MPI.rank(dlf.mpi_comm_world())
 try:
     problem.displacement.eval(vals, x)
     print("(rank %i) vals + x = " % rank, vals + x)
