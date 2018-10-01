b307fb296583bfa16ef7f7185d7b3e89a5268a57
Miguel A. Rodriguez <miguelr@berkeley.edu>
2018-10-01 12:01:57 -0700

    Added argparse to problem 2 and evaluation of final location of apex.

--- test/manual/land_benchmark/problem2.py
+++ test/manual/land_benchmark/problem2.py
@@ -1,18 +1,43 @@
+import os
+import sys
+import argparse
 import dolfin as dlf
 import fenicsmechanics as fm
 
-# Optimization options for the form compiler
-dlf.parameters['form_compiler']['cpp_optimize'] = True
-dlf.parameters['form_compiler']['representation'] = "uflacs"
-dlf.parameters['form_compiler']['quadrature_degree'] = 3
-dlf.parameters['form_compiler']['optimize'] = True
+# For use with emacs python shell.
+try:
+    sys.argv.remove("--simple-prompt")
+except ValueError:
+    pass
 
-rank = dlf.MPI.rank(dlf.mpi_comm_world())
-if rank !=0:
-    dlf.set_log_level(dlf.ERROR)
+parser = argparse.ArgumentParser()
+parser.add_argument("--pressure",
+                    default=10.0, type=float,
+                    help="Pressure to be applied at z = 0.")
 
-# --- GLOBAL VARIABLES --------------------------------------------------------
-# output_dir = args.output_dir
+default_mesh_dir = fm._get_mesh_file_names("ellipsoid", ret_dir=True, ret_mesh=False)
+default_mesh_file = os.path.join(default_mesh_dir, "ellipsoid_1000um.h5")
+default_fiber_file = os.path.join(default_mesh_dir, "fibers/n%i-p0-1000um.xml.gz")
+default_fiber_files = [default_fiber_file % i for i in [1, 2]]
+parser.add_argument("--mesh-file",
+                    type=str, default=default_mesh_file,
+                    help="Name of mesh file to use for mesh, facet function, " \
+                    + "and fiber directions.")
+parser.add_argument("--fiber-files",
+                    default=default_fiber_files, nargs=2, type=str,
+                    help="Name of files for vector fiber directions.")
+parser.add_argument("--incompressible",
+                    action="store_true", help="Model as incompressible material.")
+parser.add_argument("--bulk-modulus",
+                    type=float, default=1e3, dest="kappa",
+                    help="Bulk modulus of the material.")
+parser.add_argument("--loading-steps", "-ls",
+                    type=int, default=10,
+                    help="Number of loading steps to use.")
+parser.add_argument("--polynomial-degree", "-pd",
+                    type=int, default=2, dest="pd", choices=[1, 2, 3],
+                    help="Polynomial degree to be used for displacement.")
+args = parser.parse_args()
 
 # Region IDs
 HNBC  = 0  # homogeneous Neumann BC
@@ -20,74 +45,87 @@ HDBC  = 10  # homogeneous Dirichlet BC
 INBC  = 20  # inhomogeneous Neumann BC
 
 # Time parameters
-loading_steps = 100
-t0, tf = tspan = [0., 1.]
-dt = (tf - t0)/loading_steps
-theta = 1.0
-beta = 0.25
-gamma = 0.5
-save_freq = 1
-
-pressure = dlf.Expression('10.0*t', degree=2, t=t0)
+t0, tf = interval = [0., 1.]
+dt = (tf - t0)/args.loading_steps
 
 # Material subdictionary
-mat_dict = {
+material = {
     'const_eqn': 'guccione',
     'type': 'elastic',
-    'incompressible': False,
+    'incompressible': args.incompressible,
     'density': 0.0,
-    'kappa': 1e3,
+    'kappa': args.kappa,
     'bt': 1.0,
     'bf': 1.0,
     'bfs': 1.0,
     'C': 10.0,
     'fibers': {
-        'fiber_files': ['fibers/n1-p0.xml.gz',
-                        'fibers/n2-p0.xml.gz'],
+        'fiber_files': args.fiber_files,
         'fiber_names': ['n1', 'n2'],
         'element': 'p0'
-        }
     }
+}
 
 # Mesh subdictionary
-# mesh_dict = {'mesh_file': 'new-ellipsoid_300um.h5',
-#              'boundaries': 'new-ellipsoid_300um.h5'}
-mesh_dict = {'mesh_file': 'ellipsoid_300um.h5',
-             'boundaries': 'ellipsoid_300um.h5'}
+mesh = {
+    'mesh_file': args.mesh_file,
+    'boundaries': args.mesh_file
+}
 
 # Formulation subdictionary
-formulation_dict = {
-    'time':{
-        'unsteady': True,
-        'theta': theta,
-        'beta': beta,
-        'gamma': gamma,
-        'dt': dt,
-        'interval': tspan
-        },
+formulation = {
+    'time':{'dt': dt, 'interval': interval},
     'domain': 'lagrangian',
-    'element': 'p1',
     'inverse': False,
     'bcs':{
         'dirichlet': {
             'displacement': [[0.]*3],
             'regions': [HDBC],
             'velocity': [[0.]*3]
-            },
+        },
         'neumann': {
             'regions': [INBC],
             'types': ['pressure'],
-            'values': [pressure]
-            }
+            'values': ["%f*t" % args.pressure]
         }
     }
+}
+
+if args.incompressible:
+    formulation['element'] = "p%i-p%i" % (args.pd, args.pd - 1)
+else:
+    formulation['element'] = "p%i" % args.pd
 
 # Overall configuration
-config = {'material': mat_dict,
-          'mesh': mesh_dict,
-          'formulation': formulation_dict}
+config = {
+    'material': material,
+    'mesh': mesh,
+    'formulation': formulation
+}
 
-result_file = 'results-guccione-tmp/ellipsoid-fm.pvd'
+if args.incompressible:
+    fname_disp = "results/displacement-incompressible.pvd"
+    fname_pressure = "results/pressure.pvd"
+else:
+    fname_disp = "results/displacement.pvd"
+    fname_pressure = None
 problem = fm.SolidMechanicsProblem(config)
-solver = fm.SolidMechanicsSolver(problem, fname_disp=result_file)
-solver.full_solve(save_freq=save_freq)
+solver = fm.SolidMechanicsSolver(problem, fname_disp=fname_disp,
+                                 fname_pressure=fname_pressure)
+solver.full_solve()
+
+rank = dlf.MPI.rank(dlf.mpi_comm_world())
+if rank == 0:
+    print("DOF(u) = ", problem.displacement.function_space().dim())
+
+import numpy as np
+vals = np.zeros(3)
+x_endocardium = np.array([0., 0., -17.])
+x_epicardium = np.array([0., 0., -20.])
+try:
+    problem.displacement.eval(vals, x_endocardium)
+    print("(rank %i, endocardium) vals + x = " % rank, vals + x_endocardium)
+    problem.displacement.eval(vals, x_epicardium)
+    print("(rank %i, endocardium) vals + x = " % rank, vals + x_epicardium)
+except RuntimeError:
+    pass
